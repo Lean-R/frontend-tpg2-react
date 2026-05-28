ef0fae446444672336c34cec348adc05e465168b
Leandro Ezequiel Rocha <leandro_rocha93@hotmail.com>
2026-05-28 16:32:26 -0300

    feat: implementar galeria de imagenes grid con lightbox

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "@components/home/hero-section";
 import TeamSection from "@components/home/team-section";
+import WorkspaceGallery from "../../components/WorkspaceGallery";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <TeamSection />
+      <WorkspaceGallery />
     </div>
   );
 };
