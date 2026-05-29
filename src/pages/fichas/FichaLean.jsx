30a51040fd1001b30d38428fcff7da5859a9f3ec
Luma2001 <lumaqui2001@yahoo.com.ar>
2026-05-29 14:06:44 -0300

    pages fichas

--- src/pages/fichas/FichaLean.jsx
+++ src/pages/fichas/FichaLean.jsx
@@ -1,8 +1,37 @@
+import Profile from "@components/profile"
+import { useState,useEffect } from "react";
+
+
+
+
 const FichaLean = () => {
+
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+
+  useEffect(() => {
+    fetch("/data/lean-data.json")
+      .then((response) => {
+        if (!response.ok) throw new Error("Error al cargar el perfil")
+        return response.json()
+      })
+      .then((jsonData) => {
+        setData(jsonData);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al cargar el perfil:", error);
+        setLoading(false);
+      });
+  }, []);
+
+
   return (
-    <div>
-      LEAN
-    </div>
+    <>
+      {loading ? <p>Cargando perfil...</p> : <Profile data={data} />}
+    </>
   )
 }
 
