b01fb709a5cfa77af303ba82749cbf6b627430ca
Leandro Ezequiel Rocha <leandro_rocha93@hotmail.com>
2026-05-28 22:50:10 -0300

    feat: implementar cinta tipo marquee para reviews de clientes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import HeroSection from "@components/home/hero-section";
 import TeamSection from "@components/home/team-section";
 import WorkspaceGallery from "../../components/WorkspaceGallery";
+import MarqueeClientes from "../../components/MarqueeClientes";
 import ContactForm from "../../components/ContactForm";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection />
       <TeamSection />
       <WorkspaceGallery />
+      <MarqueeClientes />
       <ContactForm />
     </div>
   );
