8b4bdc0004f8a4b79ffb0044de302c491c4ec0d5
Leandro Ezequiel Rocha <leandro_rocha93@hotmail.com>
2026-05-28 21:26:41 -0300

    fix: corregir __dirname en vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+// Reemplazo nativo para __dirname en ESM
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-    resolve: {
+  resolve: {
     alias: {
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@assets": path.resolve(__dirname, "src/assets"),
     },
   },
-})
+});
